f6a0f235baba9dcf72d0bae6b7d106258a0a2ef2
Bobby <M.Babati574@Gmail.Com>
2025-08-30 11:37:06 +0330

    Refactor App structure to integrate routing and improve layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import "./App.css";
+import React from "react";
+import { useRoutes, useLocation } from "react-router-dom";
+import AppRoute from "./config/app-route.jsx";
+import { slideToggle } from "./composables/slideToggle.js";
 
-function App() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  );
-}
+export default function App() {
+  const element = useRoutes(AppRoute);
+  const location = useLocation();
 
-export default App;
+  // on every route change
+  React.useEffect(() => {
+    const elm = document.querySelector(".app");
+    if (elm) {
+      elm.classList.remove("app-sidebar-mobile-toggled");
+    }
+    const elm2 = document.querySelector(".app-top-nav");
+    if (elm2 && (elm2 as HTMLElement).style.display === "block") {
+      slideToggle(document.querySelector(".app-top-nav"));
+    }
+  }, [location]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
+// Bootstrap
+import "bootstrap";
+
+// css
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "react-perfect-scrollbar/dist/css/styles.css";
+import "./index.css";
+import "./scss/styles.scss";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
